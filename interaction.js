3544538a00774dcc74dfb9e763e99a26cbb6cf1b
LiamSmith7 <unconfigured@null.spigotmc.org>
2022-01-24 16:38:33 +0000

    TODO: Add JavaScript to make the website more interactive

---
+++ interaction.js
@@ -0,0 +1,32 @@
+/*
+==============================================
+TODO
+==============================================
+- Javascript, and how to add/modify/remove elements within the page (jQuery)
+- Research Z Index - Things belonging to the container div is culled when positioned outside the bounds of the container div
+
+
+==============================================
+Main menu
+==============================================
+
+
+
+==============================================
+Noughts & Crosses
+==============================================
+Squares:
+Button size -> 28% * 3 = 84%
+Margin size -> 4% * 4 = 16%
+
+.rotated {
+  transform: rotate(45deg);
+}
+
+let className = "nc";
+for(let i = 1; i <= 9; i++){
+    let idName = "nc" + i;
+    create(div, idName, className) // ?
+}
+
+*/
